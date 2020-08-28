77c36cfd80fe110a923692af67b357a48aa80aa3
pranjalpokharel7 <pranjalpokharel007@gmail.com>
2020-08-28 14:24:49 +0545

    intial commit with resources

---
+++ client/src/components/NavPages/MainPage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const MainPage = () => {
+    return (
+        <div style={textStyle}>
+            This is MEDAID App.
+        </div>
+    )
+}
+
+const textStyle = {
+    color: "#cc0000",
+    fontFamily: "Raleway-Bold",
+    fontSize: "20px",
+}
+
+export default MainPage
