cfd2f43199eea226c98fcae6856940a235556f48
imsanskar <sanskaramgain@gmail.com>
2020-08-28 20:22:48 +0545

    Todo for Pysical exercise updated

--- client/src/components/NavPages/Physical.js
+++ client/src/components/NavPages/Physical.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Navigation from '../CommonComponents/Navigation'
 import Heading from '../CommonComponents/Heading'
+import ExerciseForm from '../SpecificComponents/ExerciseForm'
+import TodoWrapper from '../CommonComponents/TodoWrapper'
 
 const Physical = () => {
+    const flag = 'exercise'
     return (
         <div>
             <Navigation />
-            <Heading headingTitle="Physical"/>
+            <Heading headingTitle="Physical" />
+            <TodoWrapper title='' flag={flag} />
         </div>
     )
 }
