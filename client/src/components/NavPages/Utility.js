38b3bc9c380bff9db882a322710e943fd3698895
pranjalpokharel7 <pranjalpokharel007@gmail.com>
2020-08-29 07:17:01 +0545

    random-quote-fetcher

--- client/src/components/NavPages/Utility.js
+++ client/src/components/NavPages/Utility.js
@@ -3,6 +3,7 @@ import Navigation from '../CommonComponents/Navigation'
 import Heading from '../CommonComponents/Heading'
 import BlockText from '../CommonComponents/BlockText'
 import UtilityForm from '../SpecificComponents/Forms/UtilityForm'
+import QuoteFetcher from '../SpecificComponents/QuoteFetcher'
 
 const Utility = () => {
     const [menuShow, updateMenuStatus] = useState(false)
@@ -25,7 +26,7 @@ const Utility = () => {
             <span className="hidden-form-mm" hidden>
                 <UtilityForm />
             </span>
-
+            <QuoteFetcher />
         </>
     )
 }
